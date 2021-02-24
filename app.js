a758328ca2725f4cf0d62920fdc30750ae26e2b8
samuchakraborty <samuckb@gmail.com>
2021-02-24 21:15:15 +0600

    native node driver for crud operation

---
+++ app.js
@@ -0,0 +1,56 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'fruitDB';
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+// Use connect method to connect to the Server
+client.connect(function (err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+
+    findDocuments(db, function () {
+        client.close();
+    });
+});
+
+const insertDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Insert some documents
+    collection.insertMany([
+        {
+            name: "Apple", review: 5
+        }, { name: "Banna", review: 5 },
+        { name: "Kola", review: 5 }
+    ], function (err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+}
+
+
+
+const findDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Find some documents
+    collection.find({}).toArray(function (err, fruit) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(fruit)
+        callback(fruit);
+    });
+}
